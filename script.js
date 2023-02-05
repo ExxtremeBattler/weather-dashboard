761b8b8b957cdf43ba22f49cadaf9583520b98dd
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-02-05 13:58:47 +0000

    completed ajax request flow

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
 $( document ).ready(function(){
     
-    var latitude
-    var longitude
     let searchButton = document.getElementById("search-button")
-    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+latitude+"&lon="+longitude+
-    "&appid=c5f801d62e4ccdab988d155cc4462710"
-
+    
     searchButton.addEventListener("click", function(event) {
         event.preventDefault()
+        
         var city = document.getElementById("search-input").value
+        var latitude
+        var longitude
         let geoURL = "http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=5&appid=c5f801d62e4ccdab988d155cc4462710"
         console.log(city);
+
 
         $.ajax({
             url: geoURL,
             method: "GET"
           }).then(function(response){
-            console.log(response[0].lat, response[0].lon)
+            latitude = response[0].lat
+            longitude = response[0].lon
+            let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+latitude+"&lon="+longitude+
+    "&appid=c5f801d62e4ccdab988d155cc4462710"
+            console.log(latitude, longitude)
 
-            // $.ajax({
-            //     url: queryURL,
-            //     method: "GET"
-            //   }).then(function(response){
 
-            //   })
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+              }).then(function(response){
+                console.log(response)
+
+              })
 
           }
     )})
